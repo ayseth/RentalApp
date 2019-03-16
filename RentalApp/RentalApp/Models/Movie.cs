40d37f60f3f0c7d5de1d781d5b2cec763f34f96c
A.Y Seth <aysait101@gmail.com>
2019-03-16 20:29:06 +0000

    first commit

---
+++ RentalApp/RentalApp/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
