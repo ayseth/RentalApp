d4adfd64dc8af2a5941ebf23fc027271cb4604aa
A.Y Seth <aysait101@gmail.com>
2019-03-19 18:48:10 +0000

    added theme

--- RentalApp/RentalApp/App_Start/BundleConfig.cs
+++ RentalApp/RentalApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace RentalApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
