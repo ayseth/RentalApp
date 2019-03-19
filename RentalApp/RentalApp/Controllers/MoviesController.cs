d14da23aa0868f521fc4a8bae608a564cdeb69c0
A.Y Seth <aysait101@gmail.com>
2019-03-19 19:21:16 +0000

    action parameter try

--- RentalApp/RentalApp/Controllers/MoviesController.cs
+++ RentalApp/RentalApp/Controllers/MoviesController.cs
@@ -16,5 +16,22 @@ namespace RentalApp.Controllers
 
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
     }
 }
