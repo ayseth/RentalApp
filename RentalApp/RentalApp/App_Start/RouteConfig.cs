a1b284393f2f5aece566cc5bd19ee989b521f8c7
A.Y Seth <aysait101@gmail.com>
2019-03-21 17:27:20 +0000

    2nd routing added

--- RentalApp/RentalApp/App_Start/RouteConfig.cs
+++ RentalApp/RentalApp/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace RentalApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", Action = "ByReleaseDate"},
+                new { year = @"\d{4}", month = @"\d{2}"});
+
+
+        routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- RentalApp/RentalApp/Controllers/MoviesController.cs
+++ RentalApp/RentalApp/Controllers/MoviesController.cs
@@ -17,21 +17,12 @@ namespace RentalApp.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
+            return Content(year + "/" + month);
         }
+       
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-
-        }
+        
     }
 }
