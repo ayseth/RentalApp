e0fc333ca30c56caff87289802512e7e14a65003
A.Y Seth <aysait101@gmail.com>
2019-03-22 08:31:58 +0000

    wrong approaches to pass data to views

--- RentalApp/RentalApp/Controllers/MoviesController.cs
+++ RentalApp/RentalApp/Controllers/MoviesController.cs
@@ -14,9 +14,13 @@ namespace RentalApp.Controllers
         {
             var movie = new Movie() {Name = "Shrek!"};
 
-            return View(movie);
+            ViewData["randomMovie"] = movie;
+            ViewBag.Movie = movie; //movie property is added to run bag at runtime, so no compile time safety
+
+            return View();
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
