8c4910939edce66506c2882e5a0c35d29c35d94b
A.Y Seth <aysait101@gmail.com>
2019-03-22 12:45:28 +0000

    razor syntax

--- RentalApp/RentalApp/Controllers/MoviesController.cs
+++ RentalApp/RentalApp/Controllers/MoviesController.cs
@@ -29,7 +29,7 @@ namespace RentalApp.Controllers
             return View(viewModel);
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12}")]
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
