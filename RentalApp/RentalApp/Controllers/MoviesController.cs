c0a00119f36ade1f968e39e2d0f7b6160a58607f
A.Y Seth <aysait101@gmail.com>
2019-03-23 11:25:37 +0000

    addded nav pages

--- RentalApp/RentalApp/Controllers/MoviesController.cs
+++ RentalApp/RentalApp/Controllers/MoviesController.cs
@@ -10,8 +10,24 @@ namespace RentalApp.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);  
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+
+        }
         // GET: Movies/Random
-        public ActionResult Random()
+       /* public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
             var customers = new List<Customer>
@@ -27,7 +43,7 @@ namespace RentalApp.Controllers
             };
 
             return View(viewModel);
-        }
+        }*/
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
