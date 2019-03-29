42499941970ab2c025da456e137ba242357a359d
A.Y Seth <aysait101@gmail.com>
2019-03-29 16:44:49 +0000

    customer: replaced 'magic numbers'/ applied static fields

--- RentalApp/RentalApp/Models/MembershipType.cs
+++ RentalApp/RentalApp/Models/MembershipType.cs
@@ -14,5 +14,9 @@ namespace RentalApp.Models
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
         public string Name { get; set; }
+
+        public static readonly byte Unknown = 0; //readonly so we don't change the value accidently elsewhere in the code
+        public static readonly byte PayAsYouGo = 1;
+
     }
 }
