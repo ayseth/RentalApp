40d824b8bcda9deff3c00a2e3298701e6bb66508
A.Y Seth <aysait101@gmail.com>
2019-04-02 19:39:29 +0100

    customers: changed to IHttpActionResult

--- RentalApp/RentalApp/Controllers/Api/CustomersController.cs
+++ RentalApp/RentalApp/Controllers/Api/CustomersController.cs
@@ -26,22 +26,24 @@ namespace RentalApp.Controllers.Api
         }
 
         //GET/api/customers/1
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
 
-            return Mapper.Map<Customer, CustomerDto>(customer);          //can't use selecct() as we return only one customer object
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));          //can't use selecct() as we return only one customer object
         } 
 
         // POST/api/customers
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto) //IHttpActionResult similar to actionresult in mvc framework
         {
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
+
+      
 
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
@@ -49,7 +51,7 @@ namespace RentalApp.Controllers.Api
 
             customerDto.Id = customer.Id;
 
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
 
         //PUT/api/customers/1
