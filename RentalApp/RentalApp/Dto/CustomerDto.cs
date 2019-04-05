d38c4fccac3a474101c3ceffcc74d64fc67e93c7
A.Y Seth <aysait101@gmail.com>
2019-04-05 10:19:31 +0100

    customers: returning hierarchical data

--- RentalApp/RentalApp/Dto/CustomerDto.cs
+++ RentalApp/RentalApp/Dto/CustomerDto.cs
@@ -19,6 +19,8 @@ namespace RentalApp.Dto
 
         public byte MembershipTypeId { get; set; }
 
+        public MembershipTypeDto MembershipType { get; set; }
+
         /*[Min18YearsIfAMember]*/
         public DateTime? Birthday { get; set; }
     }
