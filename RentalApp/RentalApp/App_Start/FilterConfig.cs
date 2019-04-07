072db454c93ee0a401e099d9910a9a77450319cb
A.Y Seth <aysait101@gmail.com>
2019-04-07 15:04:11 +0100

    SSL enables and revoked access from other ports

--- RentalApp/RentalApp/App_Start/FilterConfig.cs
+++ RentalApp/RentalApp/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace RentalApp
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
