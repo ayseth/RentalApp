35f320021ce2fea6a73d93b638bffd7c12302f9e
A.Y Seth <aysait101@gmail.com>
2019-04-24 21:37:16 +0100

    edge case: defensive approach

--- RentalApp/RentalApp/Controllers/Api/NewRenatlsController.cs
+++ RentalApp/RentalApp/Controllers/Api/NewRenatlsController.cs
@@ -17,14 +17,24 @@ namespace RentalApp.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No movie Id have been added")
+
             var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
+            if (customer == null)
+                return BadRequest("Customer Id not valid")
 
             var movies = _context.Movies.Where(
                 m => newRental.MovieIds.Contains(m.Id));
 
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more movies are invalid")
+
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie not available")
                 var rental = new Rental
                 {
                     Customer = customer,
